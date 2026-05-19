ef9677b3d52c96169f4f3c787134f5a39bdd21ec
Cassanova27 <ramat2552@gmail.com>
2026-05-19 20:07:09 +0100

    added first name and last name to db, removed username

--- database.py
+++ database.py
@@ -8,7 +8,8 @@ def create_database():
     cursor.execute("""
         CREATE TABLE IF NOT EXISTS users (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
-            username TEXT NOT NULL UNIQUE,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
             email TEXT NOT NULL UNIQUE,
             password TEXT NOT NULL
         )
